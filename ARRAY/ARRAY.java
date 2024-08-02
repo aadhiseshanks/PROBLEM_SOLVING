69da3c586337a0d0d6b0958cf93e271d31e02a30
AADHISESHAN K S <83263942+aadhiseshanks@users.noreply.github.com>
2024-08-02 13:02:19 +0530

    .

---
+++ ARRAY/ARRAY.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+        for(int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        int j = 0;
+        String s = sc.next();
+        for(int i = 0; i < s.length(); i++) {
+            if(s.charAt(i) == 'R') {
+                if (j < n - 1) j++;
+            }
+            else if(s.charAt(i) == 'L') {
+                arr[j] = arr[j] + 1;
+            }
+            else if(s.charAt(i) == 'D') {
+                arr[j] = arr[j] - 1;
+            }
+            else if(s.charAt(i) == 'T') {
+                if (j > 0) j--;
+            }
+            else if(s.charAt(i) == 'S') {
+                if (i + 1 < s.length() && Character.isDigit(s.charAt(i + 1))) {
+                    int x = Character.getNumericValue(s.charAt(i + 1));
+                    if (x >= 0 && x < n) {
+                        int y = arr[j];
+                        arr[j] = arr[x];
+                        arr[x] = y;
+                    }
+                }
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
