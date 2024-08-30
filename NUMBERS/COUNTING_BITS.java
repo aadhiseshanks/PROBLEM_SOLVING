8619dfc429177d17418b13fd9297b10e7ef8e587
AADHISESHAN K S <83263942+aadhiseshanks@users.noreply.github.com>
2024-08-30 16:17:23 +0530

    .

---
+++ NUMBERS/COUNTING_BITS.java
@@ -0,0 +1,19 @@
+// Sample Input:
+// 5
+// Sample Output:
+// 1 1 2 1 2
+
+import java.util.Scanner;
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int size = sc.nextInt();
+        int arr[] = new int[size+1];
+        for(int i=1; i<=size; i++) {
+            arr[i] = arr[(i&(i-1))]+1;
+        }
+        for(int i=1; i<=size; i++) {
+            System.out.print(arr[i]+" ");
+        }
+    }
+}
