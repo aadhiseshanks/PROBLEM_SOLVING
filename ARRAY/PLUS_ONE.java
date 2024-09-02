3f10f82d38a6fd61ce66582a178951b9263012d8
AADHISESHAN K S <83263942+aadhiseshanks@users.noreply.github.com>
2024-09-02 13:46:42 +0530

    .

---
+++ ARRAY/PLUS_ONE.java
@@ -0,0 +1,46 @@
+// Leetcode - 66
+
+// Example 1:
+
+// Input: digits = [1,2,3]
+// Output: [1,2,4]
+// Explanation: The array represents the integer 123.
+// Incrementing by one gives 123 + 1 = 124.
+// Thus, the result should be [1,2,4].
+
+// Example 2:
+
+// Input: digits = [4,3,2,1]
+// Output: [4,3,2,2]
+// Explanation: The array represents the integer 4321.
+// Incrementing by one gives 4321 + 1 = 4322.
+// Thus, the result should be [4,3,2,2].
+
+// Example 3:
+
+// Input: digits = [9]
+// Output: [1,0]
+// Explanation: The array represents the integer 9.
+// Incrementing by one gives 9 + 1 = 10.
+// Thus, the result should be [1,0].
+
+import java.util.Scanner;
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int size = sc.nextInt();
+        int arr[] = new int[size];
+        for(int i=0; i<size; i++) {
+            arr[i] = sc.nextInt();
+        }
+        int carry = 1;
+        for (int i = size - 1; i >= 0; i--) {
+            int sum = arr[i] + carry;
+            arr[i] = sum % 10;
+            carry = sum / 10;
+        }
+        for(int i=0; i<size; i++) {
+            System.out.print(arr[i]+" ");
+        }
+    }
+}
