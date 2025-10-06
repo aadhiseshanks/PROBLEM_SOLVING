7145765605e692cf206e492765f791815bf88138
AADHISESHAN K S <83263942+aadhiseshanks@users.noreply.github.com>
2025-10-06 20:13:13 +0530

    Update PASCAL'S_TRIANGLE.java

--- PATTERNS/PASCAL'S_TRIANGLE.java
+++ PATTERNS/PASCAL'S_TRIANGLE.java
@@ -12,22 +12,23 @@ import java.util.Scanner;
 class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int numRows = sc.nextInt();
-        int triangle[][] = new int[numRows][];
-        for(int i=0; i<numRows; i++) {
-            triangle[i] = new int[i+1];
+        int num = sc.nextInt();
+        int arr[][] = new int[num][];
+        for(int i=0; i<num; i++) {
+            arr[i] = new int[i+1];
             for(int j=0; j<=i; j++) {
                 if(j==0 || j==i) {
-                    triangle[i][j] = 1;
+                    arr[i][j] = 1;
                 }
-                else {
-                    triangle[i][j] = triangle[i - 1][j - 1] + triangle[i - 1][j];
-                }
+                else arr[i][j] = arr[i-1][j-1] + arr[i-1][j];
             }
         }
-        for(int i=0; i<numRows; i++) {
+        for(int i=0; i<num; i++) {
+            for(int j=0; j<num-i-1; j++) {
+                System.out.print(" ");
+            }
             for(int j=0; j<=i; j++) {
-                System.out.print(triangle[i][j]+" ");
+                System.out.print(arr[i][j]+" ");
             }
             System.out.println();
         }
