54b73619ba74d781d43bfe6294985345bd1b793e
AADHISESHAN K S <83263942+aadhiseshanks@users.noreply.github.com>
2025-10-09 19:34:31 +0530

    .

--- PATTERNS/RHOMBUS_PATTERN.java
+++ PATTERNS/RHOMBUS_PATTERN.java
@@ -1,4 +1,15 @@
 // Rhombus Patterns
+/*
+     1
+    212
+   32123
+  4321234
+ 543212345
+  4321234
+   32123
+    212
+     1
+*/
 import java.util.Scanner;
 class patrnq9 {
     public static void main(String[] args) {
