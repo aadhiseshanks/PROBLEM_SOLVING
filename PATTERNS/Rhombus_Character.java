48714a8f40d7a0ccadd571d1d039d2cc0921127f
AADHISESHAN K S <83263942+aadhiseshanks@users.noreply.github.com>
2025-10-09 20:01:04 +0530

    .

---
+++ PATTERNS/Rhombus_Character.java
@@ -0,0 +1,39 @@
+// Rhombus Character Pattern
+/*
+  A
+ ABA
+ABCBA
+ ABA
+  A
+*/
+import java.util.Scanner;
+class patrnq10 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt(); 
+        for(int i=1; i<=num; i++) { 
+            for(int j=i; j<=num; j++) {
+                System.out.print(" ");
+            }
+            for(int j=0; j<i; j++) { 
+                System.out.print((char)('A'+j)); 
+            }
+            for(int j=i-2; j>=0; j--) { 
+                System.out.print((char)('A'+j));
+            }
+            System.out.println();
+        }
+        for(int i=num-1; i>=0; i--) { 
+            for(int j=i; j<=num; j++) {
+                System.out.print(" ");
+            }
+            for(int j=0; j<i; j++) { 
+                System.out.print((char)('A'+j)); 
+            }
+            for(int j=i-2; j>=0; j--) { 
+                System.out.print((char)('A'+j)); 
+            }
+            System.out.println();
+        }
+    }
+}
