775ed97aae344069d438bebe662383405689f7c6
AADHISESHAN K S <83263942+aadhiseshanks@users.noreply.github.com>
2025-10-10 19:25:09 +0530

    Update RHOMBUS_CHARACTER.java

--- PATTERNS/RHOMBUS_CHARACTER.java
+++ PATTERNS/RHOMBUS_CHARACTER.java
@@ -7,7 +7,7 @@ ABCBA
   A
 */
 import java.util.Scanner;
-class patrnq10 {
+class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int num = sc.nextInt(); 
